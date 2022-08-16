686e5fc04bc40c35df59453479ed14ae1a8f8c9f
Kuren <kuren@MacBook-Pro.local>
2022-08-16 17:19:02 +0800

    feat: add week1

---
+++ week1/index.js
@@ -0,0 +1,13 @@
+console.log(1 < 2 < 3)
+console.log(3 < 2 < 1)
+
+
+for (var i = 0; i < 5; i++) {
+  setTimeout(() => {
+    console.log(i)
+  }, 0)
+}
+
+
+console.log(a)
+let a = 10
---
+++ week1/week1.md
@@ -0,0 +1,13 @@
+「回傳」跟「輸出」的差別
+
+js 的編譯階段與執行階段
+
+var、let 和 const 的差別
+
+什麼是 hoisting
+
+單執行緒(single threaded) 同步執行(synchronous execution)
+
+AJAX
+
+Async & Await
